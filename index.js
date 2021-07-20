7846dc9daaf641ee7adc861ecd6d83f0fd6b30cb
Dario <santillandario823@gmail.com>
2021-07-20 20:24:27 -0300

    add sessions with cookie-session

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
 const morgan = require('morgan');
 
 const app = express();
@@ -19,39 +20,37 @@ const logger = (req, res, next) => {
 app.set('view engine', 'pug'); //seteamos pug como el motor de vistas por defecto
 app.set('views', 'views'); //seteamos la carpeta views como el lugar defecto  donde van a estar los archivos html
 
-app.use(express.urlencoded());
-app.use(express.static('public'));
-app.use(morgan('tiny'))
-app.use(logger);
+app.use(express.urlencoded({extended: true}));
+app.use(cookieSession({
+    secret: 'una_cadena_secreta',
+    maxAge: 24 * 60 * 60 * 1000
+}));
+app.use('/static', express.static('public'));
+app.use(morgan('dev'))
 
 //////////////////////////////////
 // ROUTAS
 //////////////////////////////////
 
+//MUESTRA LA LISTA DE NOTA
 app.get('/', (req, res) => {
-    const {name, age} = req.query;
-    const notas = ['Nota 1', 'Nota 2', 'Nota 3'];
-    res.render('index', {notas});
+    const notes = req.session.notes || [];
+    res.render('index', { notes });
 });
 
+//MUESTRA EL FORMULARIO PARA CREAR NOTAS
 app.get('/notes/new', (req, res) => {
-    res.render('new')
+    res.render('new');
 });
 
+//PERMITE CREAR NOTAS
 app.post('/notes', (req, res) => {
-    console.log(`El titulo de la nota es ${req.body.title}`);
+    req.session.id = (req.session.id || 0) + 1;
+    const id = req.session.id;
+
+    req.session.notes = req.session.notes || [];
+    req.session.notes.push({ id: id, title: req.body.title, body: req.body.body})
     res.redirect('/');
-});
-
-app.post('/users', (req, res) => {
-    res.status(404)
-    .set('Notas-version', '1.0')
-    .send('No se encontro el recurso');
-});
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Algo salio mal');
 });
 
 app.listen(3001, () => console.log('Listening on port 3001'));
